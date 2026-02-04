44f418154a0a37fb269680d106f614fee100a7bc
Hannah Jung <hj.hannahjung@gmail.com>
2026-02-04 00:10:08 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
+require("dotenv").config();
+
+const app = express();
+const MONGODB_URI = process.env.MONGODB_URI;
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/api", indexRouter);
+
+const mongoURI = MONGODB_URI;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("Mongoose connected");
+  })
+  .catch((err) => {
+    console.log("DB connection failed", err);
+  });
+
+app.listen(5000, () => {
+  console.log("Server is running on 5000");
+});
