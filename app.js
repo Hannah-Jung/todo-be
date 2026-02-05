9375fd7122508b21dcee3b4f77a4ed99da578e94
Hannah Jung <hj.hannahjung@gmail.com>
2026-02-05 03:31:32 -0800

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/api", indexRouter);
-
+const PORT = process.env.PORT || 5000;
 const mongoURI = MONGODB_URI;
 
 mongoose
@@ -22,6 +22,6 @@ mongoose
     console.log("DB connection failed", err);
   });
 
-app.listen(5000, () => {
-  console.log("Server is running on 5000");
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
